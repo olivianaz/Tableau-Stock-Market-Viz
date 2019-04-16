10a6fcbca189e4b9529b438141ab6101adc91b86
Oliviana <oliviana@localhost>
2019-04-15 23:27:17 -0700

    Refactor fetch using async...await

--- read_tableau_stock_data.js
+++ read_tableau_stock_data.js
@@ -1,34 +1,36 @@
-const url = 'https://query.data.world/s/m532vzhcaksmm3dbtbt3vqwpgo275f';
+async function fetchTableauStockData(){
+  const startURL = 'https://query.data.world/s/m532vzhcaksmm3dbtbt3vqwpgo275f';
+  const startFetchResult = fetch(startURL);
+  const startResponse = await startFetchResult;
+  const newFetchResult = fetch(startResponse.url);
+  const rawData = await newFetchResult;
+  const dataStr = await rawData.text();
 
-fetch(url)
-  .then(function(data) {
-    console.log(data);
-    var new_url = data.url;
-    console.log("fetching: " + new_url);
-    fetch(new_url)
-      //.then((resp) => resp.json())
-      .then(function(data) {
-        return data.text();
-      })
-      .then(function(data) {
-        var allTextLines = data.split(/\r\n|\n/);
-        var headers = allTextLines[0].split(',');
-        var lines = [];
+  var allTextLines = dataStr.split(/\r\n|\n/);
+  var headers = allTextLines[0].split(',');
+  var lines = [];
 
-        for (var i=1; i<allTextLines.length; i++) {
-          var data = allTextLines[i].split(',');
-          if (data.length == headers.length) {
-            var tarr = [];
-            for (var j=0; j<headers.length; j++) {
-              tarr.push(data[j]);
-            }
-            lines.push(tarr);
-          }
-        }
-        console.table(headers);
-        console.table(lines);
-      })
-    })
-  .catch(function(error) {
-    console.log(error);
-  });
+  for (var i=1; i<allTextLines.length; i++) {
+    var data = allTextLines[i].split(',');
+    if (data.length == headers.length) {
+      var tarr = [];
+      for (var j=0; j<headers.length; j++) {
+        tarr.push(data[j]);
+      }
+      lines.push(tarr);
+    }
+  }
+  console.table(headers);
+  console.table(lines);
+  return lines;
+}
+
+async function processData(){
+  const fetchData = fetchTableauStockData();
+  const dataset = await fetchData;
+  const adjCloseColumn = 5;
+  console.log("Length: " + dataset.length);
+
+}
+
+processData();
