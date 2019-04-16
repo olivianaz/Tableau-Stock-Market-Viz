4176666561ec5bfe45ea9f3efb0c0570e93385f3
Oliviana <oliviana@localhost>
2019-04-16 12:30:53 -0700

    Create scatterplot of closing prices

--- read_tableau_stock_data.js
+++ read_tableau_stock_data.js
@@ -30,7 +30,69 @@ async function processData(){
   const dataset = await fetchData;
   const adjCloseColumn = 5;
   console.log("Length: " + dataset.length);
+  var adjClose = [];
 
+  for (var i=0; i<dataset.length; i++){
+    adjClose.push(parseFloat(dataset[i][adjCloseColumn]));
+  }
+  console.log("AdjClose: ");
+  console.log(adjClose);
+
+  const w = 1000;
+  const h = 600;
+  const padding = 40;
+
+  const xScale = d3.scaleLinear()
+                   .domain([0, dataset.length - 1])
+                   .range([padding, w - padding]);
+
+  const yScale = d3.scaleLinear()
+                   .domain([0, d3.max(dataset, (d) => parseFloat(d[adjCloseColumn]))])
+                   .range([h - padding, padding]);
+
+  const svg = d3.select("svg")
+                // Add your code below this line
+                .attr("width", w)
+                .attr("height", h);
+
+  // add scatter plot
+  svg.selectAll("circle")
+     .data(dataset)
+     .enter()
+     .append("circle")
+     // Add your code below this line
+     .attr("cx", (d, i) => xScale(i) )
+     .attr("cy", (d, i) => yScale(parseFloat(d[adjCloseColumn])))
+     .attr("r", 2);
+
+   // add labels
+   svg.selectAll("text")
+      .data(dataset)
+      .enter()
+      .append("text")
+      // Add your code below this line
+      .text((d, i) => {
+        if (i==0 || i == dataset.length - 1){
+          return parseInt(d[adjCloseColumn]);
+        }
+      })
+      .attr("x", (d, i) => xScale(i + 5))
+      .attr("y", (d, i) => yScale(parseFloat(d[adjCloseColumn])));
+
+   // add x and y axes
+   const xAxis = d3.axisBottom(xScale);
+   // Add your code below this line
+   const yAxis = d3.axisLeft(yScale);
+   // Add your code above this line
+
+   svg.append("g")
+      .attr("transform", "translate(0," + (h - padding) + ")")
+      .call(xAxis);
+
+   // Add your code below this line
+   svg.append("g")
+      .attr("transform", "translate(" + padding + ", 0)")
+      .call(yAxis);
 }
 
 processData();
